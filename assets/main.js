619e5f31e3e96ae1a2effec8afb671a5cc5bd2ee
Thiago Calazans <calazansthi@gmail.com>
2019-03-19 17:52:08 -0300

    Add logo rotation when scrolling at the top

--- assets/main.js
+++ assets/main.js
@@ -6,18 +6,27 @@ window.onload = function(){
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
-    //Exibição do header quanto à rolagem da página
+    //Observação da rolagem da página
     var prevScrollpos = window.pageYOffset; 
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset; 
         header = document.getElementById("header-id");
-        headerHeight = header.offsetHeight; console.log(headerHeight);
+        headerHeight = header.offsetHeight;        
         
+        //Exibição do header quanto à rolagem
         if (prevScrollpos > currentScrollPos) {
             header.style.transform = "translateY(0px)";
         } else if (prevScrollpos < currentScrollPos && currentScrollPos > headerHeight + headerHeight) {            
             header.style.transform = "translateY(-"+ headerHeight +"px)";
         }
         prevScrollpos = currentScrollPos; 
+
+        //Gira logo ao subir página
+        logo = document.getElementById("svg-logo");
+        if (currentScrollPos <= 15){            
+            logo.classList.add("rotate");
+        } else {
+            logo.classList.remove("rotate");
+        }
     }
 };
