77158d5593ae7d53777897d4c1a69c13409ed00d
Thiago Calazans <calazansthi@gmail.com>
2019-10-08 16:53:11 -0300

    Improves logic of logo animation

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,10 @@
 window.onload = function(){
     header = document.getElementById("header-id");
     logo = document.getElementById("svg-logo");
-
-    header.onmouseover = function(){
-        logo.classList.remove("rotate");
-    };
+    
 
     //Rolar página ao clicar no header
-    document.getElementById("header-id").onclick = function() {
+    header.onclick = function() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
@@ -21,16 +18,21 @@ window.onload = function(){
         //Exibição do header quanto à rolagem
         if (prevScrollpos > currentScrollPos) {
             header.style.transform = "translateY(0px)";
-        } else if (prevScrollpos < currentScrollPos && currentScrollPos > headerHeight + headerHeight) {            
-            header.style.transform = "translateY(-"+ headerHeight +"px)";
-        }
-        
-        //Gira logo ao subir página        
-        if (prevScrollpos > currentScrollPos && currentScrollPos <= 15){            
-            logo.classList.add("rotate");            
-        } else {
-            logo.classList.remove("rotate");
-        }
+            
+            //Gira logo ao chegar no topo
+            if (currentScrollPos <= 15){            
+                logo.classList.add("rotate");
+                
+                //Remove classe rotate ao terminar transition
+                rotate = document.querySelector(".rotate");
+                style = getComputedStyle(rotate);                     
+                setTimeout(function(){ 
+                    logo.classList.remove("rotate")
+                }, parseFloat(style.transitionDuration) * 1000);       
+            }            
+        } else if (prevScrollpos < currentScrollPos && currentScrollPos > headerHeight * 2) {            
+            header.style.transform = "translateY(-"+ headerHeight +"px)";            
+        }       
 
         prevScrollpos = currentScrollPos; 
     }
